d8c583214d7806959ec061a33eed9e23e2102443
surreymakis <mg00219@surrey.ac.uk>
2017-02-10 22:34:25 +0000

    select destination

--- app/src/main/java/uk/ac/gla/idi/beaconexample/MainActivity.java
+++ app/src/main/java/uk/ac/gla/idi/beaconexample/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
         FloatingActionButton toggleScan = (FloatingActionButton) findViewById(R.id.btnStartTrip);
         toggleScan.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SelectDestinationActivity.class);
+                Intent intent = new Intent(MainActivity.this, SelectLineActivity.class);
                 startActivity(intent);
             }
         });
