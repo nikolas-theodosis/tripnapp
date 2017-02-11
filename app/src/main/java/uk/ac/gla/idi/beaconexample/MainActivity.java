451073ef5bd73de0418e591adbe67c6d4b8c857f
Nikos Theodosis <nikolas.theodosis@gmail.com>
2017-02-11 18:21:48 +0000

    Make main activity ask for ble access

--- app/src/main/java/uk/ac/gla/idi/beaconexample/MainActivity.java
+++ app/src/main/java/uk/ac/gla/idi/beaconexample/MainActivity.java
@@ -1,5 +1,8 @@
 package uk.ac.gla.idi.beaconexample;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,11 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity{
-    private Button toggleScan = null;
+
+    private BluetoothAdapter bleDev = null;
+    // request ID for enabling Bluetooth
+    private static final int REQUEST_ENABLE_BT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,12 @@ public class MainActivity extends AppCompatActivity{
         FloatingActionButton toggleScan = (FloatingActionButton) findViewById(R.id.btnStartTrip);
         toggleScan.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SelectLineActivity.class);
-                startActivity(intent);
+                final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+                bleDev = bluetoothManager.getAdapter();
+                if (bleDev == null || !bleDev.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
             }
         });
     }
@@ -39,6 +47,25 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((requestCode == REQUEST_ENABLE_BT) && (resultCode == RESULT_OK))
+        {
+            Intent intent = new Intent(MainActivity.this, SelectLineActivity.class);
+            startActivity(intent);
+            boolean isEnabling = bleDev.enable();
+            if (!isEnabling)
+            {
+                // an immediate error occurred - perhaps the bluetooth is already on?
+            }
+            else if (bleDev.getState() == BluetoothAdapter.STATE_TURNING_ON)
+            {
+                // the system, in the background, is trying to turn the Bluetooth on
+                // while your activity carries on going without waiting for it to finish;
+                // of course, you could listen for it to finish yourself - eg, using a
+                // ProgressDialog that checked mBluetoothAdapter.getState() every x
+                // milliseconds and reported when it became STATE_ON (or STATE_OFF, if the
+                // system failed to start the Bluetooth.)
+            }
+        }
     }
 
     @Override
