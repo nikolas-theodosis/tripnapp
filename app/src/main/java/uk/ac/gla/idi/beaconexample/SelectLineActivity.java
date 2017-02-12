51748c4a79724e73d0636f1be5c280e267ef56ef
Nikos Theodosis <nikolas.theodosis@gmail.com>
2017-02-12 15:06:01 +0000

    Update activity with bundles for fragments and input from files

--- app/src/main/java/uk/ac/gla/idi/beaconexample/SelectLineActivity.java
+++ app/src/main/java/uk/ac/gla/idi/beaconexample/SelectLineActivity.java
@@ -3,20 +3,31 @@ package uk.ac.gla.idi.beaconexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectLineActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private String DEPARTURE_STATION = null;
+    private ArrayList<String> stations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_destination);
-
-
+        loadStationsFromFile();
+        DEPARTURE_STATION = getIntent().getStringExtra("DEPARTURE_STATION");
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
@@ -27,9 +38,34 @@ public class SelectLineActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new InnerDestinationsFragment(), getResources().getString(R.string.tab_inner));
-        adapter.addFragment(new OuterDestinationsFragment(), getResources().getString(R.string.tab_outer));
+        InnerDestinationsFragment inner = new InnerDestinationsFragment();
+        OuterDestinationsFragment outer = new OuterDestinationsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("DEPARTURE_STATION", DEPARTURE_STATION);
+        bundle.putStringArrayList("STATIONS", stations);
+        inner.setArguments(bundle);
+        outer.setArguments(bundle);
+        adapter.addFragment(inner, getResources().getString(R.string.tab_inner));
+        adapter.addFragment(outer, getResources().getString(R.string.tab_outer));
         viewPager.setAdapter(adapter);
+    }
+
+    private void loadStationsFromFile() {
+        BufferedReader reader;
+        String record;
+        String[] data;
+        InputStream ins = null;
+        try {
+            ins = getApplicationContext().getResources().getAssets().open("stations");
+            reader = new BufferedReader(new InputStreamReader(ins));
+            record = reader.readLine();
+            while (record != null){
+                stations.add(record);
+                record = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
