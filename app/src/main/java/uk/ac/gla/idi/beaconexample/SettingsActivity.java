2baf5577542ff0d22c4151005fdf47bd1d8b3c89
Nikos Theodosis <nikolas.theodosis@gmail.com>
2017-02-22 01:24:33 +0000

    Fix number picker

--- app/src/main/java/uk/ac/gla/idi/beaconexample/SettingsActivity.java
+++ app/src/main/java/uk/ac/gla/idi/beaconexample/SettingsActivity.java
@@ -1,9 +1,16 @@
 package uk.ac.gla.idi.beaconexample;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 
 public class SettingsActivity  extends PreferenceActivity {
@@ -18,15 +25,28 @@ public class SettingsActivity  extends PreferenceActivity {
             Preference myPref = findPreference("pref_number_of_stations");
             myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference preference) {
-                    Spinner spinner = (Spinner) findViewById(R.id.alarm_spinner);
-                    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                            R.array.number_of_stations_array, android.R.layout.simple_spinner_item);
-                    // Specify the layout to use when the list of choices appears
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    // Apply the adapter to the spinner
-                    spinner.setAdapter(adapter);
-
-                    /*Toast.makeText(getApplicationContext(), "A", Toast.LENGTH_SHORT).show();*/
+                    LayoutInflater inflater = SettingsActivity.this.getLayoutInflater();
+                    View view= inflater.inflate(R.layout.dialog_stations, null);
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(SettingsActivity.this);
+                    dialog.setTitle(R.string.dialog_stops_before);
+                    dialog.setMessage(R.string.dialog_stops_before_message);
+                    dialog.setView(view);
+                    NumberPicker np = (NumberPicker) view.findViewById(R.id.numberPicker);
+                    np.setMaxValue(13);
+                    np.setMinValue(0);
+                    np.setWrapSelectorWheel(false);
+                    dialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //save the value to property file
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
                     return true;
                 }
             });
