fcfc8a641ee8450d2055fac73cb45491c53f4de6
Panos Papatheodosiou <papatheodosiou.panagiotis@outlook.com>
2017-02-24 09:27:26 +0000

    Update on list item click

--- app/src/main/java/uk/ac/gla/idi/beaconexample/OuterDestinationsFragment.java
+++ app/src/main/java/uk/ac/gla/idi/beaconexample/OuterDestinationsFragment.java
@@ -1,11 +1,13 @@
 package uk.ac.gla.idi.beaconexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,7 @@ public class OuterDestinationsFragment extends ListFragment implements AdapterVi
     private ArrayList<String> stations = new ArrayList<>();
     private ArrayList<String> nextStops = new ArrayList<>();
     private String DEPARTURE_STATION = null;
+    private String DESTINATION_STATION = null;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -40,6 +43,19 @@ public class OuterDestinationsFragment extends ListFragment implements AdapterVi
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         // TODO implement some logic
+        DESTINATION_STATION=getListAdapter().getItem(position).toString();
+        //l.getChildAt(position).setBackgroundColor(Color.argb(255, 255,0 ,0));
+        Toast.makeText(getContext(),getListAdapter().getItem(position).toString()+" Is selected",Toast.LENGTH_LONG).show();
+
+        if (position != 0) {
+            Intent intent = new Intent(getActivity(), TripMonitorActivity.class);
+            intent.putExtra("DEPARTURE_STATION",DEPARTURE_STATION);
+            intent.putExtra("DESTINATION_STATION",DESTINATION_STATION);
+            intent.putExtra("DIRECTION","Outer");
+
+
+            startActivity(intent);
+        }
     }
 
     @Override
